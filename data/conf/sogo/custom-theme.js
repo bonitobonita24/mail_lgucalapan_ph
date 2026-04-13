e1edb0bb3a255fdf7cf9fcbcb2f05bb87ebc338d
Bonito Bonita <bonitobonita24@gmail.com>
2026-04-14 00:18:08 +0800

    Refactor code structure for improved readability and maintainability

--- data/conf/sogo/custom-theme.js
+++ data/conf/sogo/custom-theme.js
@@ -5,28 +5,39 @@
 
   configure.$inject = ['$mdThemingProvider'];
   function configure($mdThemingProvider) {
-    var orangePalette = $mdThemingProvider.extendPalette('orange', {
-      '500': 'f28237',
-      '600': 'e0742e',
-      '700': 'cc6826',
-      '800': 'b85c1f',
-      'A200': 'f28237',
-      'A400': 'e0742e',
-      'A700': 'cc6826',
+    var navyPalette = $mdThemingProvider.extendPalette('blue', {
+      '500': '0d244c',
+      '600': '091b38',
+      '700': '061229',
+      '800': '040d1c',
+      'A200': '0d244c',
+      'A400': '091b38',
+      'A700': '061229',
       'contrastDefaultColor': 'light'
     });
-    $mdThemingProvider.definePalette('mailcow-orange', orangePalette);
+    $mdThemingProvider.definePalette('calapan-navy', navyPalette);
+    var greenPalette = $mdThemingProvider.extendPalette('green', {
+      '500': '1a6b3c',
+      '600': '155a31',
+      '700': '114a28',
+      '800': '0d3a1f',
+      'A200': '1a6b3c',
+      'A400': '155a31',
+      'A700': '114a28',
+      'contrastDefaultColor': 'light'
+    });
+    $mdThemingProvider.definePalette('calapan-green', greenPalette);
     $mdThemingProvider.theme('default')
-      .primaryPalette('mailcow-orange', {
+      .primaryPalette('calapan-navy', {
         'default': '500',
         'hue-1': '600',
         'hue-2': '700',
         'hue-3': 'A700'
       })
-      .accentPalette('blue-grey', {
-        'default': '600',
-        'hue-1': '300',
-        'hue-2': '300',
+      .accentPalette('calapan-green', {
+        'default': '500',
+        'hue-1': '600',
+        'hue-2': '700',
         'hue-3': 'A700'
       })
       .backgroundPalette('grey');
